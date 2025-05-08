be0111aecaebc28ee338a5ac20b6bdf4dcf6d367
be00b06e-1ea5-4259-bc65-d39f19d38b3b <daredamon@hotmail.com>
2025-05-08 08:19:29 +0000

    Project created

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <xc.h>
+int i=0;
+
+void sub1(void)
+{
+        int x;
+        x=x+10;
+}
+
+void main(void) {
+    // initialise
+    while(1)
+    {
+        i++;
+
+    }
+    //return;
+}
